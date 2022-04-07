fcd4d7d05da2c3f53004d977866fac54ff5028a7
Josuke <y.motosugi0227@gmail.com>
2022-04-07 19:26:40 +1000

    Promote the duplicated NumberFormat class usage to a field for a reusability.

--- src/com/company/MortgageReport.java
+++ src/com/company/MortgageReport.java
@@ -4,10 +4,12 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -16,12 +18,12 @@ public class MortgageReport {
         System.out.println("----------------");
 
         for (double balance : calculator.getRemainingBalances())
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
     }
 
     public void printMonthlyPayment() {
         double monthlyPayment = calculator.calculateMortgage();
-        String monthlyPaymentFormatted = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+        String monthlyPaymentFormatted = currency.format(monthlyPayment);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
